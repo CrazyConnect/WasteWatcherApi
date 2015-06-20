1743d527db9d26de4b5a3eee9977271392139263
bxav <xavier.b@zoho.com>
2015-06-20 12:53:35 +0200

    Add new users scenary

--- features/api/users.feature
+++ features/api/users.feature
@@ -3,9 +3,33 @@ Feature: Access to the api
   Scenario: List users
     Given I prepare a GET request on "/api/users"
     When I send the request
+    Then print the last response
     Then I should receive a 200 json response
     And scope into the first "data" property
+    And the properties exist:
+    """
+    username
+    """
+
+  Scenario: Show an user
+    Given I prepare a GET request on "/api/users/joe86"
+    When I send the request
     Then print the last response
+    Then I should receive a 200 json response
+    And scope into the "data" property
+    And the properties exist:
+    """
+    username
+    lon
+    lat
+    """
+
+  Scenario: List users filter in square area
+    Given I prepare a GET request on "/api/users?lat1=1&lon1=1&lat2=2&lon2=2"
+    When I send the request
+    Then print the last response
+    Then I should receive a 200 json response
+    And scope into the first "data" property
     And the properties exist:
     """
     username
--- src/AppBundle/Controller/Api/UserController.php
+++ src/AppBundle/Controller/Api/UserController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -18,13 +19,31 @@ class UserController extends Controller
      * @Route("/users")
      * @Method("GET")
      */
-    public function usersAction()
+    public function indexAction(Request $request)
     {
+        $debugQueryString = $request->getQueryString();
 
         $user = new \stdClass();
         $user->username = "coucou";
         $obj = new \stdClass();
         $obj->data = [$user, clone $user];
+        $obj->debugQueryString = $debugQueryString;
+        return new JsonResponse($obj);
+    }
+
+    /**
+     * @Route("/users/{username}")
+     * @Method("GET")
+     */
+    public function showAction($username)
+    {
+
+        $user = new \stdClass();
+        $user->username = $username;
+        $user->lon = "1.333";
+        $user->lat = "42.4242";
+        $obj = new \stdClass();
+        $obj->data = $user;
         return new JsonResponse($obj);
     }
 }
